3b98ee8449071619b8336bea71cf9ae17fef5307
Michael Sokol <mikaa123@gmail.com>
2014-06-11 15:49:17 +0200

    Adding groups: simple registry for resources

---
+++ test/group_test.js
@@ -0,0 +1,42 @@
+var assert = require('assert'),
+	should = require('should'),
+	sinon = require('sinon'),
+	makeGroup = require('../').makeGroup,
+	makeResource = require('../').makeResource;
+
+describe('Group', function () {
+	describe('Create a group', function () {
+		it('Should create a new group', function () {
+			makeGroup().should.be.ok;
+		});
+	});
+
+	describe('Adding resources', function () {
+		var group;
+
+		beforeEach(function () {
+			group = makeGroup();
+		});
+
+		it('Should allow adding one or many resources', function () {
+			var one = makeResource('one', '/one'),
+				two = makeResource('two', '/two'),
+				three = makeResource('three', '/three');
+
+			group.add(one);
+			group.add(two, three);
+
+			group('one').should.eql(one);
+			group('two').should.eql(two);
+			group('three').should.eql(three);
+		});
+
+		it("Should throw if the Resource isn't available", function () {
+			[undefined, null, 'coffee'].forEach(function (v) {
+				assert.throws(function () {
+					group(v);
+				});
+			});
+		});
+	});
+});
