11795ce07eb40894f7804f33ebf541eff6957b0a
Michael Sokol <mikaa123@gmail.com>
2014-07-13 18:36:07 +0200

    using routers to attach resources

--- bin/catnap_runner.js
+++ bin/catnap_runner.js
@@ -1,16 +1,22 @@
 var express = require('express'),
 	app = express(),
-	port = process.argv[2] || 3000;
+	port = process.argv[2] || 3000,
+	router = express.Router();
 
-app.listen(port, function () {
-	console.log('Server running on port', port);
-})
-	.on('error', function () {
-		console.log('Port %s already used.', port);
-	});
+app
+	.use(router)
+	.use(function(req, res, next) {
+		res.send(404, 'Sorry cant find that!');
+	})
+	.listen(port, function () {
+		console.log('Server running on port', port);
+	})
+		.on('error', function () {
+			console.log('Port %s already used.', port);
+		});
 
 // Catnap's facade
-GLOBAL.catnap = require('../lib/facade')(app);
+GLOBAL.catnap = require('../lib/facade')(router);
 
 // Walk each folders to find *-resource.js files
 var readdirp = require('readdirp');
@@ -18,10 +24,4 @@ var readdirp = require('readdirp');
 var stream = readdirp({ root: process.cwd(), fileFilter: '*-resource.js' })
 	.on('readable', function () {
 		require(stream.read().fullPath);
-	})
-	.on('end', function () {
-		app.use(function(req, res, next) {
-			res.send(404, 'Sorry cant find that!');
-		});
-
 	});
